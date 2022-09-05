49ee48c77e93e3eea0c0f27e5f7e0223a8b43310
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-09-05 11:36:29 -0300

    typo fix of numeroConta variable, previous numeroContal and now numeroConta

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,12 +1,12 @@
 class Contact {
   final int id;
   final String nome;
-  final int numeroContal;
+  final int numeroConta;
 
-  Contact(this.id, this.nome, this.numeroContal);
+  Contact(this.id, this.nome, this.numeroConta);
 
   @override
   String toString() {
-    return 'Contact{id: $id, name: $nome, numeroConta: $numeroContal}';
+    return 'Contact{id: $id, name: $nome, numeroConta: $numeroConta}';
   }
 }
