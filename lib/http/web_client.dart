2b40fead39f6412c4dd07053f6f58b8650038244
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-09-05 15:11:11 -0300

    create the post request method to save a transactions in the webapi, refactor the the webapi url and the the client

--- lib/http/web_client.dart
+++ lib/http/web_client.dart
@@ -10,7 +10,7 @@ class LoggingInterceptor implements InterceptorContract {
   Future<RequestData> interceptRequest({RequestData? data}) async {
     print('Request');
     print('url: ${data!.url}');
-    // print('headers: ${data.headers}');
+    print('headers: ${data.headers}');
     print('body: ${data.body}');
     return data;
   }
@@ -19,18 +19,21 @@ class LoggingInterceptor implements InterceptorContract {
   Future<ResponseData> interceptResponse({ResponseData? data}) async {
     print('Response');
     print('status code: ${data!.statusCode}');
-    // print('headers: ${data.headers}');
+    print('headers: ${data.headers}');
     print('body: ${data.body}');
     return data;
   }
 
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
+
+  final String baseUrl = 'http://192.168.0.9:8080/transactions';
+
   Future<List<Transaction>> findAllWebClient() async {
-    final Client client = InterceptedClient.build(
-      interceptors: [LoggingInterceptor()],
-    );
     final Response response = await client
         .get(
-          Uri.parse('http://192.168.0.9:8080/transactions'),
+          Uri.parse(baseUrl),
         )
         .timeout(const Duration(seconds: 5));
 
@@ -50,5 +53,37 @@ class LoggingInterceptor implements InterceptorContract {
       transactions.add(transaction);
     }
     return transactions;
+  }
+
+  Future<Transaction> save(Transaction transaction) async {
+    final Map<String, dynamic> transactionMap = {
+      'value': transaction.valor,
+      'contact': {
+        'name': transaction.contato.nome,
+        'accountNumber': transaction.contato.numeroConta,
+      }
+    };
+
+    final String transactionJson = jsonEncode(transactionMap);
+
+    final Response response = await client.post(
+      Uri.parse(baseUrl),
+      headers: {
+        'Content-type': 'application/json',
+        'password': '1000',
+      },
+      body: transactionJson,
+    );
+
+    Map<String, dynamic> json = jsonDecode(response.body);
+    final Map<String, dynamic> contactJson = json['contact'];
+    return Transaction(
+      Contact(
+        0,
+        contactJson['accountNumber'],
+        contactJson['name'],
+      ),
+      json['value'],
+    );
   }
 }
