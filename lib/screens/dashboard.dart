681b3583714794c5bdc74f78166c2e0f4d2a14ce
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-09-05 11:42:51 -0300

    add the const keyword before the TransactionsList() widget at the _showTransactionsList() method

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -58,7 +58,7 @@ void _showContactsList(BuildContext context) {
 void _showTransactionsList(BuildContext context) {
   Navigator.of(context).push(
     MaterialPageRoute(
-      builder: (context) => TransactionsList(),
+      builder: (context) => const TransactionsList(),
     ),
   );
 }
