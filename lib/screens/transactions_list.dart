56be7b2ceceac73ffd0acbadefc5553f9348d3e4
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-09-05 11:53:00 -0300

    create the FutureBuilder widget to listen to the findAllWebClient() function at the LogginInterceptor() widget, manage all the asycronous exceptions with switch - case ConnectionStates and return a CenteredMessage when the snapshot data is not avaiable to the user, and show the transaction list when the data is avaiable from the webapi

--- lib/screens/transactions_list.dart
+++ lib/screens/transactions_list.dart
@@ -1,40 +1,68 @@
 import 'package:flutter/material.dart';
 
-import 'package:bytebank_persistence/models/transaction.dart';
+import '../components/centered_message.dart';
+import '../components/progress.dart';
+import '/http/web_client.dart';
+import '/models/transaction.dart';
 
 class TransactionsList extends StatelessWidget {
-  TransactionsList({super.key});
+  const TransactionsList({super.key});
 
-  final List<Transaction> transactions = [];
   @override
   Widget build(BuildContext context) {
+    final webClient = LoggingInterceptor();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Transações'),
       ),
-      body: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (context, index) {
-          final Transaction transaction = transactions[index];
+      body: FutureBuilder<List<Transaction>>(
+        initialData: const <Transaction>[],
+        future: webClient.findAllWebClient(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return const Progress();
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              if (snapshot.hasData) {
+                final List<Transaction> transactions = snapshot.data!;
+                if (transactions.isNotEmpty) {
+                  return ListView.builder(
+                    itemCount: transactions.length,
+                    itemBuilder: (context, index) {
+                      final Transaction transaction = transactions[index];
+                      return Card(
+                        child: ListTile(
+                          leading: const Icon(Icons.monetization_on),
+                          title: Text(
+                            transaction.valor.toString(),
+                            style: const TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            transaction.contato.numeroConta.toString(),
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+              }
+              return const CenteredMessage(
+                message: 'No transactions found',
+                icon: Icons.warning,
+              );
+          }
 
-          return Card(
-            child: ListTile(
-              leading: const Icon(Icons.monetization_on),
-              title: Text(
-                transaction.valor.toString(),
-                style: const TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: Text(
-                transaction.contato.numeroContal.toString(),
-                style: const TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-          );
+          return const CenteredMessage(message: 'Unknown error');
         },
       ),
     );
