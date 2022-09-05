bbb5c38ca09c12dd1b734d08670685fad1089666
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-09-05 12:04:56 -0300

    parse the response.data json to the required Transaction and Contact models, manage the exceptions - update aula 1.3

--- lib/http/web_client.dart
+++ lib/http/web_client.dart
@@ -1,12 +1,16 @@
+import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
+
+import '/models/contact.dart';
+import '/models/transaction.dart';
 
 class LoggingInterceptor implements InterceptorContract {
   @override
   Future<RequestData> interceptRequest({RequestData? data}) async {
     print('Request');
     print('url: ${data!.url}');
-    print('headers: ${data.headers}');
+    // print('headers: ${data.headers}');
     print('body: ${data.body}');
     return data;
   }
@@ -15,17 +19,36 @@ class LoggingInterceptor implements InterceptorContract {
   Future<ResponseData> interceptResponse({ResponseData? data}) async {
     print('Response');
     print('status code: ${data!.statusCode}');
-    print('headers: ${data.headers}');
+    // print('headers: ${data.headers}');
     print('body: ${data.body}');
     return data;
   }
-}
 
-void findAll() async {
-  final Client client = InterceptedClient.build(
-    interceptors: [LoggingInterceptor()],
-  );
-  final Response response = await client.get(
-    Uri.parse('http://192.168.0.4:8080/transactions'),
-  );
+  Future<List<Transaction>> findAllWebClient() async {
+    final Client client = InterceptedClient.build(
+      interceptors: [LoggingInterceptor()],
+    );
+    final Response response = await client
+        .get(
+          Uri.parse('http://192.168.0.9:8080/transactions'),
+        )
+        .timeout(const Duration(seconds: 5));
+
+    final List<dynamic> decodedJson = jsonDecode(response.body);
+    final List<Transaction> transactions = [];
+
+    for (Map<String, dynamic> transactionJson in decodedJson) {
+      final Map<String, dynamic> contatoJson = transactionJson["contact"];
+      final Transaction transaction = Transaction(
+        Contact(
+          0,
+          contatoJson["name"],
+          contatoJson["accountNumber"],
+        ),
+        transactionJson["value"],
+      );
+      transactions.add(transaction);
+    }
+    return transactions;
+  }
 }
